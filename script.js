bd4516b2d0002ac9abef7e8fa279880d4390c1f8
ಠ_ಠ <qaz.cetelic@protonmail.com>
2023-03-06 10:56:22 +0100

    Move bots to seperate file

--- script.js
+++ script.js
@@ -7,18 +7,6 @@ const colors = {
     yellow: ["00acc1", "1e88e5", "ffd600"],
 }
 
-let botOffsets = [];
-function getOffsets() {
-    let offset = Math.round(Math.random() * 7, 1);
-    if (botOffsets.includes(offset)) {
-        return getOffset();
-    }
-    else {
-        botOffsets.push(offset);
-        return offset;
-    }
-}
-
 let score = 0;
 let scoreText = document.createElement("p", );
 scoreText.innerText=('Score: '+score);
@@ -26,27 +14,6 @@ document.querySelector('#score').appendChild(scoreText);
 function addScore(){
     score++;
     scoreText.innerText=('Score: '+score);
-}
-
-function createBotUrl(colors, seed) {
-    return `https://api.dicebear.com/5.x/bottts/svg?baseColor=${colors[0]},${colors[1]},${colors[2]}&seed=${seed}`
-}
-
-function createBotElement(colorname, colors, seed, offset) {
-    const bot = document.createElement('img');
-    bot.classList.add('bot');
-    bot.src = createBotUrl(colors, seed);
-    bot.style.left = `${offset}vw`;
-    bot.setAttribute('color', colorname);
-    return bot;
-}
-
-function createBoxElement(colorname, colors) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    box.style.backgroundColor = `#${colors[2]}`;
-    box.setAttribute('color', colorname);
-    return box;
 }
 
 for (let colorsKey in colors) {
