aea6010583c7cd08d95c80bcc5613ec65f11069f
ಠ_ಠ <qaz.cetelic@protonmail.com>
2023-03-06 09:42:09 +0100

    orange to red

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const colors = {
     purple: ["00acc1", "1e88e5", "5e35b1"],
     blue: ["00acc1", "1e88e5", "039be5"],
-    orange: ["00acc1", "1e88e5", "fb8c00"],
+    red: ["00acc1", "1e88e5", "d32f2f"],
     green: ["00acc1", "1e88e5", "00c853"],
     yellow: ["00acc1", "1e88e5", "ffd600"],
 }
