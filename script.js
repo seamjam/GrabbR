bb8ed6126d9cf57a23d00a52a03334b3b392d416
ಠ_ಠ <qaz.cetelic@protonmail.com>
2023-03-06 10:08:15 +0100

    Add color attribute

--- script.js
+++ script.js
@@ -12,7 +12,8 @@ function getOffsets() {
     let offset = Math.round(Math.random() * 7, 1);
     if (botOffsets.includes(offset)) {
         return getOffset();
-    } else {
+    }
+    else {
         botOffsets.push(offset);
         return offset;
     }
@@ -22,26 +23,28 @@ function createBotUrl(colors, seed) {
     return `https://api.dicebear.com/5.x/bottts/svg?baseColor=${colors[0]},${colors[1]},${colors[2]}&seed=${seed}`
 }
 
-function createBotElement(colors, seed, offset) {
-    const bot = document.createElement('img')
-    bot.classList.add('bot')
-    bot.src = createBotUrl(colors, seed)
-    bot.style.left = `${offset}vw`
-    return bot
+function createBotElement(colorname, colors, seed, offset) {
+    const bot = document.createElement('img');
+    bot.classList.add('bot');
+    bot.src = createBotUrl(colors, seed);
+    bot.style.left = `${offset}vw`;
+    bot.setAttribute('color', colorname);
+    return bot;
 }
 
-function createBoxElement(colors) {
-    const box = document.createElement('div')
-    box.classList.add('box')
-    box.style.backgroundColor = `#${colors[2]}`
-    return box
+function createBoxElement(colorname, colors) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.backgroundColor = `#${colors[2]}`;
+    box.setAttribute('color', colorname);
+    return box;
 }
 
 for (let colorsKey in colors) {
-    let bot = createBotElement(colors[colorsKey], 1, Math.round(Math.random() * 90));
+    let bot = createBotElement(colorsKey, colors[colorsKey], 1, Math.round(Math.random() * 90));
     document.querySelector("#bots").appendChild(bot);
 }
 
 for (let colorsKey in colors) {
-    document.querySelector("#boxes").appendChild(createBoxElement(colors[colorsKey]));
+    document.querySelector("#boxes").appendChild(createBoxElement(colorsKey, colors[colorsKey]));
 }
